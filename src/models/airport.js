0d9316ae0b117ef638c4259a2cb6cbca1f2ecbdf
Yash Shivajirao Sontakke <beingyash@Yashs-MacBook.local>
2023-06-14 21:26:00 +0530

    Added aiport Models

--- src/models/airport.js
+++ src/models/airport.js
@@ -1,35 +1,42 @@
 'use strict';
-const {Model} = require('sequelize');
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
+  class Airport extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-      models.City.hasMany(models.Airport, {
+      models.Airport.belongsTo(models.City, {
         foreignKey: 'cityId',
-        as : 'airport'
+        as: 'city',
+        onDelete: 'CASCADE'
       });
     }
   }
-  City.init({
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false
-    },
+  Airport.init({
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique:true
+      unique: true
     },
+
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    cityId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+
   }, {
     sequelize,
-    modelName: 'City',
+    modelName: 'Airport',
   });
-  return City;
+  return Airport;
 };
